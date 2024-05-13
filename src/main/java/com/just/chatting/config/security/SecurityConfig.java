46fefb3c4372b8923dba18f6bb816f36a426969f
hyunsu <mypace0600@gmail.com>
2024-05-13 16:42:15 +0900

    h2-console 접근 허용처리

--- src/main/java/com/just/chatting/config/security/SecurityConfig.java
+++ src/main/java/com/just/chatting/config/security/SecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -38,9 +40,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(httpSecurityHeadersConfigurer -> httpSecurityHeadersConfigurer.frameOptions(
+                        HeadersConfigurer.FrameOptionsConfig::disable
+                ))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/", "/auth/**", "/js/**", "/css/**", "/img/**", "/WEB-INF/views/**").permitAll()
+                        .requestMatchers("/", "/auth/**", "/js/**", "/css/**", "/img/**", "/WEB-INF/views/**","/h2-console/*").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(login -> login
