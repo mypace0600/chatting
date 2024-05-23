fdbaddcc1c2c8e8586a2b74755068b293e9fc35d
hyunsu <mypace0600@gmail.com>
2024-05-23 14:40:30 +0900

    친구검색 기능 추가

--- src/main/java/com/just/chatting/controller/FriendController.java
+++ src/main/java/com/just/chatting/controller/FriendController.java
@@ -1,24 +1,28 @@
 package com.just.chatting.controller;
 
+import com.just.chatting.common.CamelCaseMap;
 import com.just.chatting.common.ResponseDto;
 import com.just.chatting.config.security.PrincipalDetail;
 import com.just.chatting.dto.FriendDto;
+import com.just.chatting.dto.FriendSearchDto;
 import com.just.chatting.entity.Friend;
+import com.just.chatting.entity.User;
 import com.just.chatting.service.FriendService;
+import com.just.chatting.service.UserService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+
 
 @Slf4j
 @Controller
@@ -26,12 +30,16 @@ import java.util.List;
 public class FriendController {
 
     private final FriendService friendService;
-    @GetMapping("/friend/list")
-    public String getFriendList(@AuthenticationPrincipal PrincipalDetail principal, Model model){
-        List<Friend> friendList = friendService.findAllFriendsOfMine(principal);
-        log.info("@@@@@@@@@ friendList :{}",friendList.toString());
-        model.addAttribute("friendList",friendList);
-        return "friend/friendList";
+    private final UserService userService;
+
+    @PostMapping("/friend/search")
+    @ResponseBody
+    public ResponseEntity<CamelCaseMap> searchFriend(@RequestBody FriendSearchDto searchDto){
+        log.debug("@@@@@@@@@@@@@@@@@@@@@@@@@@@@ searchDto :{}",searchDto.getSearchValue());
+        User searchedUser = userService.findByNickName(searchDto.getSearchValue()).orElseThrow(EntityNotFoundException::new);
+        CamelCaseMap resultBox = new CamelCaseMap();
+        resultBox.put("searchedUser",searchedUser);
+        return ResponseEntity.ok(resultBox);
     }
 
     @PostMapping("/friend/ask")
