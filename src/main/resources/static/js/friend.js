eda174303c4c7fa9107049aa096ebc2d344dde1b
hyunsu <mypace0600@gmail.com>
2024-05-23 15:00:48 +0900

    친구검색시 모달 띄워서 정보 넣기

--- src/main/resources/static/js/friend.js
+++ src/main/resources/static/js/friend.js
@@ -24,7 +24,20 @@ let friendIndex = {
             if (resp.status === 500) {
                 alert("해당 닉네임을 가진 사용자가 없습니다.");
             } else {
-                alert(resp.data);
+                // 모달 엘리먼트를 가져옵니다.
+                var modal = document.getElementById("myModal");
+                // 모달의 정보를 표시할 엘리먼트를 가져옵니다.
+                var modalInfo = document.getElementById("searchedUserInfo");
+                // 검색된 사용자 정보를 모달에 표시합니다.
+                modalInfo.textContent = "닉네임: " + resp.searchedUser.nickName + ", 이메일: " + resp.searchedUser.email;
+                // 모달을 띄웁니다.
+                modal.style.display = "block";
+
+                // 모달의 닫기 버튼을 클릭하면 모달을 닫습니다.
+                var span = document.getElementsByClassName("close")[0];
+                span.onclick = function() {
+                    modal.style.display = "none";
+                }
             }
         }).fail(function (error) {
             alert(JSON.stringify(error));
