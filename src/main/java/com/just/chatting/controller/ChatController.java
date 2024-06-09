36cbc7e731f125e2418edb6326f18ad1397e5402
mypace0600 <mypace0600@gmail.com>
2024-06-09 18:26:24 +0900

    채팅연결 사전 작업

--- src/main/java/com/just/chatting/controller/ChatController.java
+++ src/main/java/com/just/chatting/controller/ChatController.java
@@ -1,9 +1,21 @@
 package com.just.chatting.controller;
 
+import com.just.chatting.common.CamelCaseMap;
+import com.just.chatting.config.security.PrincipalDetail;
+import com.just.chatting.dto.ChatRoomDto;
+import com.just.chatting.entity.ChatRoom;
+import com.just.chatting.entity.User;
 import com.just.chatting.service.ChatService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/chat")
@@ -11,5 +23,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChatController {
 
     private final ChatService chatService;
+
+    @PostMapping("/check")
+    public ResponseEntity<CamelCaseMap> checkChatRoom(@RequestBody ChatRoomDto chatRoomDto, @AuthenticationPrincipal PrincipalDetail principal){
+        CamelCaseMap resultBox = new CamelCaseMap();
+        User loginUser = principal.getUser();
+        Optional<ChatRoom> chatRoom = chatService.findChatRoom(loginUser.getId(),chatRoomDto.getFromUserId());
+        if(chatRoom.isPresent()){
+            resultBox.put("chatRoomId",chatRoom.get().getId());
+        } else {
+            resultBox.put("chatRoomId","");
+        }
+        return ResponseEntity.ok(resultBox);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<CamelCaseMap> createChatRoom(@RequestBody ChatRoomDto chatRoomDto, @AuthenticationPrincipal PrincipalDetail principal){
+        ChatRoom chatRoom = chatService.createChatRoom(chatRoomDto.getFromUserId(),principal.getUser().getId());
+        CamelCaseMap resultBox = new CamelCaseMap();
+        resultBox.put("chatRoomId",chatRoom.getId());
+        return ResponseEntity.ok(resultBox);
+    }
 
 }
--- src/main/java/com/just/chatting/service/ChatService.java
+++ src/main/java/com/just/chatting/service/ChatService.java
@@ -1,9 +1,46 @@
 package com.just.chatting.service;
 
+import com.just.chatting.entity.ChatRoom;
+import com.just.chatting.entity.ChatRoomUser;
+import com.just.chatting.entity.User;
+import com.just.chatting.repository.ChatRoomRepository;
+import com.just.chatting.repository.ChatRoomUserRepository;
+import com.just.chatting.repository.UserRepository;
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ChatService {
+    private final ChatRoomRepository chatRoomRepository;
+    private final UserRepository userRepository;
+    private final ChatRoomUserRepository chatRoomUserRepository;
+
+    public Optional<ChatRoom> findChatRoom(Integer toUserId, Integer fromUserId) {
+        return chatRoomRepository.findByUsers(toUserId,fromUserId);
+    }
+
+    public ChatRoom createChatRoom(Integer fromUserId, Integer toUserId) {
+        User fromUser = userRepository.findById(fromUserId).orElseThrow(EntityNotFoundException::new);
+        User toUser = userRepository.findById(toUserId).orElseThrow(EntityNotFoundException::new);
+
+        ChatRoom chatRoom = new ChatRoom();
+        chatRoom = chatRoomRepository.save(chatRoom);
+
+        ChatRoomUser chatRoomUserFrom = new ChatRoomUser();
+        chatRoomUserFrom.setChatRoom(chatRoom);
+        chatRoomUserFrom.setUser(fromUser);
+        chatRoomUserRepository.save(chatRoomUserFrom);
+
+        ChatRoomUser chatRoomUserTo = new ChatRoomUser();
+        chatRoomUserFrom.setChatRoom(chatRoom);
+        chatRoomUserFrom.setUser(toUser);
+        chatRoomUserRepository.save(chatRoomUserTo);
+
+        return chatRoom;
+    }
 }
